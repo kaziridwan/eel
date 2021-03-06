fb0485d287d0d082fc8c17d0e1bb5ca0563b9ecf
kazi <ridwan.arefin@gmail.com>
2021-03-07 05:22:38 +0600

    updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const { Command } = require('commander');
+const { exec } = require('child_process');
 const program = new Command();
 program.version('0.0.1');
 
-const programArgs = program.args
-
-program.parse(process.argv);
+const programArgs = process.argv;
 
 console.log('program args',{programArgs, other: process.argv})
+console.log('running', `./teel/index.sh ${programArgs.filter((arg,index) => index > 1)}`)
+
+// exec(`./teel/index.sh ${}`)
