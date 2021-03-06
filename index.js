78d3ecaf7b94cb811f80573367c8af227bcf9882
kazi <ridwan.arefin@gmail.com>
2021-03-07 05:07:38 +0600

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
 const { Command } = require('commander');
 const program = new Command();
 program.version('0.0.1');
 
-console.log('hello!')
+console.log('kemon asos @saddat @mash!')
+program.parse(process.argv);
