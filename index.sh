61fc6afff661d53ee36b1b66b0f135d4c80eca14
kazi <ridwan.arefin@gmail.com>
2021-03-08 07:18:43 +0600

    updated

--- index.sh
+++ index.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # https://devhints.io/bash
-
-if [[ -z "$2" ]]; then
+echo $2
+if [[ -n "$2" ]]; then
   ./.teel/$2 "${@:3}"
-elif [[ -n "$2" ]]; then
+elif [[ -z "$2" ]]; then
   ./.teel/index.sh
 fi
