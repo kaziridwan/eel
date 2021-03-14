9b0b5b4dec3bf365b8d4a57e80b9fc18381e7621
kazi <ridwan.arefin@gmail.com>
2021-03-14 07:09:10 +0600

    updated

--- index.sh
+++ index.sh
@@ -4,4 +4,7 @@ if [[ -z $1 ]]; then
   ./.teel/index.sh
 elif [[ -n $1 ]]; then
   ./.teel/$1 "${@:2}"
+elif [[ $1 == "dr" && -z $2 ]]; then
+  chmod u+x ./.teel/*
+  chmod 600 ./.teel/.secrets/*
 fi
