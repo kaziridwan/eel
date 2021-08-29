3f291a51d471f67c10bb376c22dd8d135674a6d0
kazi <ridwan.arefin@gmail.com>
2021-08-29 15:34:52 +0600

    updated

---
+++ .teel/help.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is the help file TODO"
+echo "This is the help file TODO"
+# copy file to the server
+# scp file.txt remote_username@10.10.0.2:/remote/directory
+# paste a file from the server on the local machine
+# scp remote_username@10.10.0.2:/remote/file.txt /local/directory
